870ae4efcb0378cbca2383f63065d46844d3374b
AbruCrina <crina.abrudan3@gmail.com>
2023-10-29 21:19:52 +0200

    Laborator3

--- Migrations/20231026115529_BookCategory.Designer.cs
+++ Migrations/20231026115529_BookCategory.Designer.cs
@@ -4,6 +4,7 @@ using Abrudan_Crina_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Abrudan_Crina_Lab2.Migrations
 {
     [DbContext(typeof(Abrudan_Crina_Lab2Context))]
-    partial class Abrudan_Crina_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20231026115529_BookCategory")]
+    partial class BookCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
